56b243cbfdd6d13770fbb42ef74177c259a18b8c
Pierre Rolland <roll.pierre@gmail.com>
2020-05-14 22:25:07 +0200

    Small actions buttons enhancements

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -4,12 +4,20 @@ import LockIcon from '@material-ui/icons/Lock';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 export default props => (
-   <div>
-        <Fab color={ props.lockDownActive ? "primary" : "secondary" } onClick={props.onToggleLockDown}>
-            { props.lockDownActive ? <LockOpenIcon /> : <LockIcon /> }
-        </Fab>
-       <Button variant="contained" color="primary" onClick={props.onDisplayOverview}>Display overview</Button>
-       <Button variant="contained" color="primary" onClick={props.onDisplayNewly}>Display new values</Button>
-       <Button variant="contained" color="primary" onClick={props.onDisplayOverall}>Display overall values</Button>
+   <div style={{ display: 'flex' }}>
+       <div style={{ padding: '1rem' }}>
+            <Fab color={ props.lockDownActive ? "primary" : "secondary" } onClick={props.onToggleLockDown}>
+                { props.lockDownActive ? <LockOpenIcon /> : <LockIcon /> }
+            </Fab>
+       </div>
+       <div style={{ padding: '1rem' }}>
+           <Button variant="contained" color="primary" onClick={props.onDisplayOverview}>Display overview</Button>
+       </div>
+       <div style={{ padding: '1rem' }}>
+           <Button variant="contained" color="primary" onClick={props.onDisplayNewly}>Display new values</Button>
+       </div>
+       <div style={{ padding: '1rem' }}>
+           <Button variant="contained" color="primary" onClick={props.onDisplayOverall}>Display overall values</Button>
+       </div>
    </div>
 )
